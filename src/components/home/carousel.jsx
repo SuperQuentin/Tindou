5ed728b2b0ab49362a914f0869cf1ff7c58383cd
PC-DYLAN\Dylan <dylan.oliveira-ramos@cpnv.ch>
2021-03-30 16:41:15 +0200

    Have a carousel

---
+++ src/components/home/carousel.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Carousel extends Component {
+  render() {
+    return <div className="custom-header mb-5"></div>;
+  }
+}
+
+export default Carousel;
